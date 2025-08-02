ee6ef60ee51428d09a5897fd9c183792fb2faf0e
Abdussamed Kundakcı <abdussamedkundakci@gmail.com>
2025-08-03 00:08:14 +0300

    projects revision

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import turtle
 import random
 
+#Ekran
 screen = turtle.Screen()
 screen.bgcolor("light blue")
 screen.setup(800, 600)
 screen.title("Catch The Turtle")
 
+
+#Skor
 score =0
+
+#Geri sayım süre
 time_left= 30
 
+#Puan yazısı
 score_display = turtle.Turtle()
 score_display.hideturtle()
 score_display.penup()
 score_display.goto(0 ,260)
 score_display.write(f"Puan: {score}", align="center", font=("Arial", 24, "bold"))
 
+#Süre yazısı
 
 timer_display = turtle.Turtle()
 timer_display.hideturtle()
@@ -22,6 +29,7 @@ timer_display.penup()
 timer_display.goto(0, 230)
 timer_display.write(f"Kalan Süre: {time_left}", align="center", font=("Arial", 18, "bold"))
 
+#Kaplumbağa
 
 catch_turtle = turtle.Turtle()
 catch_turtle.shape("turtle")
@@ -29,6 +37,8 @@ catch_turtle.color("dark green")
 catch_turtle.penup()
 catch_turtle.speed(0)
 
+
+#Kaplumbağayı rastgele yere taşı
 def move_turtle():
     if time_left > 0:
         x = random.randint(-200, 200)
@@ -36,6 +46,7 @@ def move_turtle():
         catch_turtle.goto(x, y)
         screen.ontimer(move_turtle, 1000)
 
+#Tıkladıkça puanı artır
 def catch(x, y):
     global score
     if time_left > 0:
@@ -44,6 +55,7 @@ def catch(x, y):
         score_display.write(f"Puan: {score}", align="center", font=("Arial", 24, "bold"))
 
 
+#Geri sayım
 def countdown():
     global time_left
     if time_left > 0:
